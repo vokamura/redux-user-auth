862acdb0dc990e770e815c4e1ef695c3d51f808b
vokamura <vcokamura@gmail.com>
2018-07-11 15:36:35 -0700

    Added routes and nav bar

---
+++ src/components/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default props => {
+    return(
+        <div>
+            <h1 className="text-center">About Us</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunt ipsum suscipit nec. </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunt ipsum suscipit nec. </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunt ipsum suscipit nec. </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunt ipsum suscipit nec. </p>
+        </div>
+    )
+}
---
+++ src/components/secret_list.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default props => {
+    return (
+        <div>
+            <h1 className="text-center">Top Secret List</h1>
+            <ul>
+                <li>John</li>
+                <li>Cathi</li>
+                <li>Lizzi</li>
+                <li>Linda</li>
+                <li>Rodney</li>
+            </ul>
+        </div>
+    )
+}
